deeeeb770bde4135b32cae0ea0bd8b76775c744f
Julius Snipes <jsnipes@fb.com>
2021-07-12 17:06:24 -0400

    Added bottom navigation bar and toolbar as action bar

--- app/src/main/java/com/example/travelo/MainActivity.java
+++ app/src/main/java/com/example/travelo/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.travelo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.travelo.databinding.ActivityMainBinding;
+import com.example.travelo.fragments.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // handle navigation selection
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(
+                item -> {
+                    Fragment fragment;
+                    switch (item.getItemId()) {
+                        case R.id.action_home:
+                            fragment = new HomeFragment();
+                            break;
+                        case R.id.action_add_trip:
+                            fragment = new HomeFragment();
+                            break;
+                        case R.id.action_profile:
+                            fragment = new HomeFragment();
+                            break;
+                        case R.id.action_settings:
+                            fragment = null;
+                            break;
+                        default:
+                            fragment = null;
+                            break;
+                    }
+                    fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                    return true;
+                });
+        // Set default selection
+        binding.bottomNavigation.setSelectedItemId(R.id.action_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
