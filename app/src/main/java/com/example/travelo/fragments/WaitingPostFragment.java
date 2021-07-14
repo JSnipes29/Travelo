f01da21c5ba0b5376a96a00b9f2b92f3b2e2ea30
Julius Snipes <jsnipes@fb.com>
2021-07-14 17:40:59 -0400

    Pull to refresh for waiting list

--- app/src/main/java/com/example/travelo/fragments/WaitingPostFragment.java
+++ app/src/main/java/com/example/travelo/fragments/WaitingPostFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,6 +15,10 @@ import com.example.travelo.R;
 import com.example.travelo.adapters.NameAdapter;
 import com.example.travelo.databinding.FragmentWaitingPostBinding;
 import com.example.travelo.models.Room;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,6 +35,7 @@ public class WaitingPostFragment extends Fragment {
     FragmentWaitingPostBinding binding;
     Room room;
     NameAdapter adapter;
+    List<String> users;
 
     public WaitingPostFragment() {
         // Required empty public constructor
@@ -52,10 +58,12 @@ public class WaitingPostFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentWaitingPostBinding.inflate(getLayoutInflater(), container, false);
         View view = binding.getRoot();
+        // Get room from bundle
         room = (Room) Parcels.unwrap(getArguments().getParcelable("room"));
         JSONObject jsonUsers = room.getUsers();
-        List<String> users = new ArrayList<>();
+        users = new ArrayList<>();
         Iterator<String> iterator = jsonUsers.keys();
+        // Get a list of unready users
         while (iterator.hasNext()) {
             String key = iterator.next();
             try {
@@ -71,6 +79,55 @@ public class WaitingPostFragment extends Fragment {
         binding.rvNames.setAdapter(adapter);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         binding.rvNames.setLayoutManager(linearLayoutManager);
+
+        // Setup refresh listener which triggers new data loading
+        binding.swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                refresh();
+            }
+        });
+        // Configure the refreshing colors
+        binding.swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
         return view;
+    }
+
+    public void refresh() {
+        // Specify which class to query
+        ParseQuery<Room> query = ParseQuery.getQuery(Room.class);
+        // Specify the object id
+        query.getInBackground(room.getObjectId(), new GetCallback<Room>() {
+            public void done(Room r, ParseException e) {
+                binding.swipeContainer.setRefreshing(false);
+                if (e == null) {
+                    room = r;
+                    room.saveInBackground();
+                    JSONObject jsonUsers = r.getUsers();
+                    users.clear();
+                    Iterator<String> iterator = jsonUsers.keys();
+                    // Get a list of unready users
+                    while (iterator.hasNext()) {
+                        String key = iterator.next();
+                        try {
+                            boolean ready = jsonUsers.getBoolean(key);
+                            if (!ready) {
+                                users.add(key);
+                            }
+                        } catch (JSONException error) {
+                            Log.e(TAG, "Couldn't load ready data from server", error);
+                        }
+                    }
+                    adapter.notifyDataSetChanged();
+                } else {
+                    Log.e(TAG, "Error joining room", e);
+                }
+            }
+        });
     }
 }
